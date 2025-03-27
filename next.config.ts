b3ef1a4a88356be82d0a304e9cf3a695d45d5ddf
HRNPH <hrnph@protonmail.com>
2025-03-28 00:21:31 +0700

    feat: GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    return config;
+  },
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+    {
+      source: "/:path*.wasm",
+      headers: [
+        {
+          key: "Content-Type",
+          value: "application/wasm",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
